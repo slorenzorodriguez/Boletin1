4614378ccc488602aa674753f4f0cd9747871426
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2018-10-09 10:41:33 +0200

    commit inicial

---
+++ Boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author slorenzorodriguez
+ */
+public class Boletin1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //area triangulo = (base*altura)/2
+        int base=4, altura= 3, area;
+        area = base*altura/2;
+        System.out.println("area = "+area);
+    }
+    
+}
