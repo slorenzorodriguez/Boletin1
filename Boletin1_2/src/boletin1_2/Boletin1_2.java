42cd211e689dcd8d5b2300e0fa9660c282dfc3a7
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2018-10-09 10:49:48 +0200

    commit inicial

---
+++ Boletin1_2/src/boletin1_2/Boletin1_2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_2;
+
+/**
+ *
+ * @author slorenzorodriguez
+ */
+public class Boletin1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //area cuadrado= (lado*lado)/2
+        int lado=3, area;
+        area=lado*lado;
+        System.out.println("area = "+ area);
+    }
+    
+}
